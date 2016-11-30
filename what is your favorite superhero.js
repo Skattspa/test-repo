14cf465c15bf07797cb91cad9c1d0eb64aaf7e76
Skattspa <juan200514@gmail.com>
2016-11-30 18:51:30 +0100

    Add files via upload

---
+++ what is your favorite superhero.js
@@ -0,0 +1,31 @@
+var answer = prompt ("What is your favorite Superhero");
+
+switch (answer) {
+    case 'Spiderman':
+        document.Write("Spiderman! Our NY neighbour! ");
+        break;
+    case 'IronMan':
+        document.Write("The best armor!");
+        break;
+    case 'Batman':
+        document.Write("Master of the night! I'm Batman");
+        break;
+    case 'Superman':
+        document.Write("Watch out with the cryptonite!");
+        break;
+
+    case 'Hulk':
+        document.Write("Smash!");
+        break;
+
+    default:
+        document.Write("That is not in our database!");
+
+        break;
+    
+            };
+
+
+        }
+
+    }
